824b365282c058096cfef6e46d8e9132e7425838
Gayathry Rajeev <gayathry.rs22@gmail.com>
2025-03-23 23:16:10 +0530

    Added features for alert page

--- Flutter-My-Doctor-App/lib/pages/alert_page.dart
+++ Flutter-My-Doctor-App/lib/pages/alert_page.dart
@@ -1,65 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-class AlertPage extends StatelessWidget {
+class AlertPage extends StatefulWidget {
+  @override
+  _AlertPageState createState() => _AlertPageState();
+}
+
+class _AlertPageState extends State<AlertPage> {
+  bool _isReminderOn = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize:Size.fromHeight(500) ,
+        preferredSize: Size.fromHeight(100),
         child: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: Text.rich(
-        TextSpan(
-        children: [
-        TextSpan(
-        text: 'Health Alerts and\n',
-        style: GoogleFonts.robotoSlab(
-        fontSize: 26,
-        fontWeight: FontWeight.bold,
-        color: Colors.blue[800],
-        shadows: [
-        Shadow(
-        color: Colors.black,
-        offset: Offset(2, 2),
-    blurRadius: 4,
-    ),
-    ],
-    ),
-    ),
-    WidgetSpan(
-    alignment: PlaceholderAlignment.baseline,
-    baseline: TextBaseline.alphabetic,
-    child: Padding(
-    padding: const EdgeInsets.only(top: 0.20), // Move it lower
-    child: Text(
-    'Notifications',
-    style: GoogleFonts.robotoSlab(
-    fontSize: 26,
-    fontWeight: FontWeight.bold,
-    color: Colors.blue[800],
-      shadows: [
-        Shadow(
-          color: Colors.black,
-          offset: Offset(2, 2),
-          blurRadius: 4,
-        ),
-      ],
-    ),
-      textAlign: TextAlign.center,
-    ),
-    ),
-    ),
-    ],
-    ),
-    ),
-      ),
-      ),
-      extendBodyBehindAppBar: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'Health Alerts and\n',
+                      style: GoogleFonts.robotoSlab(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue[800],
+                        shadows: [
+                          Shadow(
+                            color: Colors.black,
+                            offset: Offset(2, 2),
+                            blurRadius: 4,
+                          ),
+                        ],
+                      ),
+                    ),
 
+
+                    WidgetSpan(
+                      alignment: PlaceholderAlignment.baseline,
+                      baseline: TextBaseline.alphabetic,
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 0.20),
+                        child: Text(
+                          'Notifications',
+                          style: GoogleFonts.robotoSlab(
+                            fontSize: 26,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue[800],
+                            shadows: [
+                              Shadow(
+                                color: Colors.black,
+                                offset: Offset(2, 2),
+                                blurRadius: 4,
+                              ),
+                            ],
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+
+      ),
+
+extendBodyBehindAppBar: true,
       body: Stack(
         children: [
-          // Background Image
           Positioned.fill(
             child: Image.asset(
               'assets/images/bg.jpg',
@@ -67,7 +81,6 @@ class AlertPage extends StatelessWidget {
             ),
           ),
 
-          // Gradient Overlay
           Positioned.fill(
             child: Container(
               decoration: BoxDecoration(
@@ -82,12 +95,64 @@ class AlertPage extends StatelessWidget {
               ),
             ),
           ),
-
-          // Page Content
-
-        ],
+          SafeArea(
+          child:Padding(padding:
+          const EdgeInsets.fromLTRB(16, 18, 16, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Medicine Reminders',
+                      style: GoogleFonts.roboto(
+                        fontSize: 26,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Switch(
+                      value: _isReminderOn,
+                      onChanged: (bool newValue) {
+                        setState(() {
+                          _isReminderOn = newValue;
+                        });
+                      },
+                      activeColor: Colors.blue[800],
+                    ),
+  ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                    Text(
+                      'Appointment Reminders',
+                      style: GoogleFonts.roboto(
+                        fontSize: 26,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Switch(
+                      value: _isReminderOn,
+                      onChanged: (bool newValue) {
+                        setState(() {
+                          _isReminderOn = newValue;
+                        });
+                      },
+                      activeColor: Colors.blue[800],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+    ),
+    ],
       ),
-    );
 
+    );
   }
 }
