a28888164b1639024a57984c6ce436e878e008af
S SIDHARTH <sidharthpunalur@gmail.com>
2025-03-30 16:59:35 +0530

    /api/chat-bot created

--- Flask-My-Doctor-Backend/app.py
+++ Flask-My-Doctor-Backend/app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask
+from routes.chatbot import chatbot_bp
 
 app = Flask(__name__)
 
+app.register_blueprint(chatbot_bp)
+
+
 @app.route('/')
 def home():
-    return 'hello'
+    return '<div style="display: flex; justify-content: center; align-items: center; height: 100vh; background-color: cyan;">' \
+           '<h1>Welcome to the myAIdoctor API!</h1>' \
+           '</div>'
 
 if __name__ == '__main__':
     app.run(debug=True)
