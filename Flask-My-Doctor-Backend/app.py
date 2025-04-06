2d060de454baef132c1a077270f4ee7046dbfb78
S SIDHARTH <sidharthpunalur@gmail.com>
2025-04-06 11:50:36 +0530

    http://127.0.0.1:5000/api/health-diagnosis add POST

--- Flask-My-Doctor-Backend/app.py
+++ Flask-My-Doctor-Backend/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template # type: ignore
 from routes.chatbot import chatbot_bp
 from routes.fitness_planner import fitness_planner_bp
+from routes.health_diagnosis import health_diagnosis_bp
 
 app = Flask(__name__)
 
 
 app.register_blueprint(chatbot_bp)
 app.register_blueprint(fitness_planner_bp)
+app.register_blueprint(health_diagnosis_bp)
 
 @app.route('/docs')
 def docs():
